18a1844d5116843d9c2a1c2660f40b7c082db902
Hermes3G <matthew@emvystudio.com>
2025-10-01 17:58:45 -0400

    Revamp

--- js/script.js
+++ js/script.js
@@ -12,3 +12,28 @@ for (i = 0; i < coll.length; i++) {
         }
     });
 }
+
+//piece background roller
+document.querySelectorAll('.piece').forEach(piece => {
+    const images = piece.dataset.images.split(',');
+    let index = 0;
+    let interval;
+
+    // set initial background
+    piece.style.backgroundImage = `url(${images[0]})`;
+
+    piece.addEventListener('mouseenter', () => {
+        index = 0;
+        interval = setInterval(() => {
+            index = (index + 1) % images.length;
+            piece.style.backgroundImage = `url(${images[index]})`;
+        }, 1500); // change every 1.5s
+    });
+
+    piece.addEventListener('mouseleave', () => {
+        clearInterval(interval);
+        index = 0;
+        piece.style.backgroundImage = `url(${images[0]})`; // reset to first image
+    });
+});
+
