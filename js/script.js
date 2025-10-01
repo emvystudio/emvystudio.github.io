62e96c90f3410810c258ebcdaf01325f754a1086
Hermes3G <matthew@emvystudio.com>
2025-10-01 19:47:33 -0400

    yupprs

--- js/script.js
+++ js/script.js
@@ -15,25 +15,28 @@ for (i = 0; i < coll.length; i++) {
 
 //piece background roller
 document.querySelectorAll('.piece').forEach(piece => {
+    if (!piece.dataset.images) return; // skip if no images defined
+
     const images = piece.dataset.images.split(',');
     let index = 0;
     let interval;
 
-    // set initial background
-    piece.style.backgroundImage = `url(${images[0]})`;
-
     piece.addEventListener('mouseenter', () => {
         index = 0;
-        interval = setInterval(() => {
-            index = (index + 1) % images.length;
+
+        startTimeout = setTimeout(() => {
             piece.style.backgroundImage = `url(${images[index]})`;
-        }, 1500); // change every 1.5s
+
+            interval = setInterval(() => {
+                index = (index + 1) % images.length;
+                piece.style.backgroundImage = `url(${images[index]})`;
+            }, 333); // cycle speed
+        }, 00); // delay before slideshow starts
     });
 
     piece.addEventListener('mouseleave', () => {
         clearInterval(interval);
-        index = 0;
-        piece.style.backgroundImage = `url(${images[0]})`; // reset to first image
+        clearTimeout(startTimeout);
+        piece.style.backgroundImage = "none";
     });
 });
-
