d5092e6cecc5a30ff0dc8fbe7e3966fd5a5c203b
Hermes3G <matthew@emvystudio.com>
2025-11-05 11:34:48 -0500

    Cleaned up project: removed duplicates and updated .gitignore

--- .eleventy.js
+++ .eleventy.js
@@ -13,4 +13,24 @@ export default function(eleventyConfig) {
         },
         htmlTemplateEngine: "njk"
     };
+
+    module.exports = function(eleventyConfig) {
+        // Create a collection for game-dev projects
+        eleventyConfig.addCollection("gameDevProjects", function(collectionApi) {
+            return collectionApi.getAll().filter(item => item.data.category === "game-dev");
+        });
+    };
+
+    eleventyConfig.addCollection("categories", collection => {
+        let categories = {};
+        collection.getAll().forEach(item => {
+            if (item.data.categories) {
+                item.data.categories.forEach(cat => {
+                    if (!categories[cat]) categories[cat] = [];
+                    categories[cat].push(item);
+                });
+            }
+        });
+        return categories;
+    });
 }
